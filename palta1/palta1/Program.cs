80af1b759639e3a81006dbf6b0e492f415a40fcb
Alumno <Alumno@PC20>
2019-08-14 12:30:04 -0500

    palta trabajo

---
+++ palta1/palta1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace palta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
